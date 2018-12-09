572bbc8a2c0c5b0b0e3a3208186869df77dfd824
AnnaGlshchnk <anna.glushchenko15@mail.ru>
2018-12-09 17:28:58 +0300

    created chat

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,42 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Client {
-    public static void main(String args[]) {
-        String str = args[0];
-        Socket socket = null;
-        BufferedReader br = null;
-        try {
-            socket = new Socket("localhost", 3345);
-            PrintStream ps = new PrintStream(socket.getOutputStream());
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            for (int i = 1; i <= 3; i++) {
-                ps.println(str);
-                System.out.println(br.readLine());
+    public static void main(String[] args) {
+        new ClientSomething("localhost", 3345);
+    }
+}
 
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("адрес недоступен" + e);
+class ClientSomething {
+
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private BufferedReader inputUser;
+    private String addr;
+    private int port;
+
+
+    public ClientSomething(String addr, int port) {
+        this.addr = addr;
+        this.port = port;
+        try {
+            this.socket = new Socket(addr, port);
         } catch (IOException e) {
-            System.err.println("ошибка I/О потока" + e);
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            System.err.println("Socket failed");
+        }
+        try {
+            inputUser = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            new ReadMsg().start();
+            new WriteMsg().start();
+        } catch (IOException e) {
+            ClientSomething.this.downService();
+        }
+    }
+
+    private void downService() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
             }
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        } catch (IOException ignored) {
+        }
+    }
+
+    private class ReadMsg extends Thread {
+        @Override
+        public void run() {
+
+            String str;
+            try {
+                while (true) {
+                    str = in.readLine();
+                    if (str.equals("stop")) {
+                        ClientSomething.this.downService();
+                        break;
+                    }
+                    System.out.println(str);
                 }
+            } catch (IOException e) {
+                ClientSomething.this.downService();
+            }
+        }
+    }
+
+    public class WriteMsg extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                String userWord;
+                try {
+                    userWord = inputUser.readLine();
+                    if (userWord.equals("stop")) {
+                        out.write("stop" + "\n");
+                        ClientSomething.this.downService();
+                        break;
+                    } else {
+                        out.write(userWord + "\n");
+                    }
+                    out.flush();
+                } catch (IOException e) {
+                    ClientSomething.this.downService();
+
+                }
+
             }
         }
     }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,71 +1,95 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
 
 public class Server {
-    public static void main(String[] args) {
+
+    public static LinkedList<ServerSomething> serverList = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(3345);
+        System.out.println("Server Started");
         try {
-            ServerSocket server = new ServerSocket(3345);
-            System.out.println("initialized");
             while (true) {
-
                 Socket socket = server.accept();
-                System.out.println(socket.getLocalPort() + " connected");
-                ServerThread thread = new ServerThread(socket);
-
-                thread.start();
+                try {
+                    serverList.add(new ServerSomething(socket));
+                } catch (IOException e) {
+                    socket.close();
+                }
             }
-        } catch (IOException e) {
-            System.err.println(e);
+        } finally {
+            server.close();
         }
     }
 }
 
-class ServerThread extends Thread {
-    private PrintStream os;
-    private BufferedReader is;
-    private InetAddress addr;
+class ServerSomething extends Thread {
 
-    public ServerThread(Socket s) throws IOException {
-        os = new PrintStream(s.getOutputStream());
-        is = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        addr = s.getInetAddress();
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+
+    public ServerSomething(Socket socket) throws IOException {
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        start();
     }
 
+    @Override
     public void run() {
-        int i = 0;
-        String str;
+        String word;
         try {
-            while ((str = is.readLine()) != null) {
-                System.out.println(str);
-                os.println("OK");
-
+            word = in.readLine();
+            try {
+                out.write(word + "\n");
+                out.flush();
+            } catch (IOException ignored) {
+            }
+            try {
+                while (true) {
+                    word = in.readLine();
+                    if (word.equals("stop")) {
+                        this.downService();
+                        break;
+                    }
+                    System.out.println("Echoing: " + word);
+                    for (ServerSomething vr : Server.serverList) {
+                        vr.send(word);
+                    }
+                }
+            } catch (NullPointerException ignored) {
             }
 
+
         } catch (IOException e) {
-            System.err.println("Disconnect");
-        } finally {
-            disconnect();
+            this.downService();
         }
     }
 
-    public void disconnect() {
+    private void send(String msg) {
         try {
-            if (os != null) {
-                os.close();
+            out.write(msg + "\n");
+            out.flush();
+        } catch (IOException ignored) {
+        }
+
+    }
+
+    private void downService() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+                for (ServerSomething vr : Server.serverList) {
+                    if (vr.equals(this)) vr.interrupt();
+                    Server.serverList.remove(this);
+                }
             }
-            if (is != null) {
-                is.close();
-            }
-            System.out.println(addr.getHostName() + " disconnecting");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            this.interrupt();
+        } catch (IOException ignored) {
         }
     }
 }
